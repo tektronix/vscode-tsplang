96009269088cafc160653314e5f06efdcfe33a24
Evan Hall <evan.tom.hall@gmail.com>
2018-08-25 19:43:45 -0400

    Updated 2450 trigger-digout

--- server/src/instrument/2450/trigger-digout.ts
+++ server/src/instrument/2450/trigger-digout.ts
@@ -17,8 +17,6 @@
 
 import { CompletionItem, CompletionItemKind, MarkupKind } from 'vscode-languageserver'
 
-/* TODO: CompletionItem.documentation.value */
-
 const triggerDigoutCompletions: Array<CompletionItem> = [
     {
         data: ['trigger'],
@@ -33,7 +31,9 @@ const triggerDigoutCompletions: Array<CompletionItem> = [
         data: ['digout', 'trigger'],
         documentation: {
             kind: MarkupKind.Markdown,
-            value: 'This function asserts a trigger pulse on one of the digital I/O lines.\n\nInitiates a trigger event and does not wait for completion. The pulse width that is set determines how long the instrument asserts the trigger.'
+            value: '```lua\nfunction assert()\n```\n\
+\n\
+Assert a trigger pulse on the currently indexed digital I/O line, returning before it has completed.'
         },
         kind: CompletionItemKind.Function,
         label: 'assert',
@@ -42,7 +42,13 @@ const triggerDigoutCompletions: Array<CompletionItem> = [
         data: ['digout', 'trigger'],
         documentation: {
             kind: MarkupKind.Markdown,
-            value: 'This attribute sets the output logic of the trigger event generator to positive or negative for the specified line.\n\nThis attribute controls the logic that the output trigger generator uses on the given trigger line.\n\nThe output state of the digital I/O line is controlled by the trigger logic, and the user-specified output state of the line is ignored.'
+            value: '```lua\ntrigger.digout[N].logic\n```\n\
+\n\
+Get or set the logic used by the output trigger generator for the currently indexed digital I/O line to \
+trigger.LOGIC_\\*. Defaults to trigger.LOGIC_NEGATIVE.\n\
+\n\
+The output state of the digital I/O line is controlled by the trigger logic. Any user-specified output state is \
+ignored.'
         },
         kind: CompletionItemKind.Property,
         label: 'logic',
@@ -51,7 +57,12 @@ const triggerDigoutCompletions: Array<CompletionItem> = [
         data: ['digout', 'trigger'],
         documentation: {
             kind: MarkupKind.Markdown,
-            value: 'This attribute describes the length of time that the trigger line is asserted for output triggers.\n\nSetting the pulse width to zero (0) seconds asserts the trigger indefinitely. To release the trigger line, use .release().'
+            value: '```lua\ntrigger.digout[N].pulsewidth\n```\n\
+\n\
+Get or set the time in seconds the currently indexed digital I/O line is asserted as a number from 0 to 100 000. \
+Defaults to +10e-6.\n\
+\n\
+When set to 0, the line is asserted until the release function is called.'
         },
         kind: CompletionItemKind.Property,
         label: 'pulsewidth',
@@ -60,7 +71,10 @@ const triggerDigoutCompletions: Array<CompletionItem> = [
         data: ['digout', 'trigger'],
         documentation: {
             kind: MarkupKind.Markdown,
-            value: 'Releases a trigger that was asserted with an indefinite pulsewidth time. It also releases a trigger that was latched in response to receiving a synchronous mode trigger. Only the specified trigger line is affected.'
+            value: '```lua\nfunction release()\n```\n\
+\n\
+Release a trigger that was asserted with an indefinite pulsewidth time. It also releases a trigger that was latched \
+in response to receiving a synchronous mode trigger. Only the currently indexed digital I/O line is affected.'
         },
         kind: CompletionItemKind.Function,
         label: 'release',
@@ -69,7 +83,9 @@ const triggerDigoutCompletions: Array<CompletionItem> = [
         data: ['digout', 'trigger'],
         documentation: {
             kind: MarkupKind.Markdown,
-            value: 'This attribute selects the event that causes a trigger to be asserted on the digital output line.\n\nThe digital trigger pulsewidth command determines how long the trigger is asserted.\n\nThe trigger stimulus for a digital I/O line can be set to one of the trigger events that are described in the following table.\n\n'
+            value: '```lua\ntrigger.digout[N].stimulus\n```\n\
+\n\
+Get or set the trigger event for the currently indexed digital I/O line to trigger.EVENT_\\*.'
         },
         kind: CompletionItemKind.Property,
         label: 'stimulus',
