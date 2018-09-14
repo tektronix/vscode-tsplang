65f400655ad37b4b848902b17d2e1860d5538f51
Evan Hall <evan.tom.hall@gmail.com>
2018-09-14 16:08:01 -0400

    Added basic Beeper unit tests

---
+++ test/server/src/instrument/provider/beeper.test.ts
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2018 Tektronix Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+// tslint:disable:no-implicit-dependencies prefer-function-over-method
+import { assert } from 'chai'
+import { suite, test } from 'mocha-typescript'
+
+import * as Beeper from '../../../../../server/src/instrument/provider/beeper'
+
+@suite class BeeperTest {
+    @test('Exports completions')
+    beeperExportsCompletions(): void {
+        assert(
+            Beeper.hasOwnProperty('completions'),
+            'Expected Beeper to export completions'
+        )
+    }
+
+    @test('Exports no completionDocs')
+    beeperExportsNoCompletionDocs(): void {
+        assert(
+            ! Beeper.hasOwnProperty('completionDocs'),
+            'Unexpected completionDocs export from Beeper'
+        )
+    }
+
+    @test('Exports signatures')
+    beeperExportsSignatures(): void {
+        assert(
+            Beeper.hasOwnProperty('signatures'),
+            'Expected Beeper to export signatures'
+        )
+    }
+}
