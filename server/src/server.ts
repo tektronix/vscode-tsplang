tektronix/vscode-tsplang

commit 4d6801db9a1a5d5d0653bbbeb53202f8623b83c3

Fixed regression of Issue #14

server/src/server.ts
@@ -266,8 +266,12 @@ connection.onSignatureHelp((params: TextDocumentPositionParams) => {
     }
 
     const results: Array<SignatureInformation> = new Array()
+
+    // add all matching signatures to the results array
     for (const signa of tspItem.commandSet.signatures) {
-        if (signa.label.indexOf(unreversed) !== -1) {
+        const signaBeforeParams: string = signa.label.slice(0, signa.labal.indexOf('('))
+
+        if (signaBeforeParams.localeCompare(unreversed) === 0) {
             results.push(signa)
         }
     }

commit 47fd68f4ccb5ceafb0553c0a820a96900b5b56a3

Fixed typo introduced in 4d6801d
And that's why you always create a branch.

server/src/server.ts
@@ -269,7 +269,7 @@ connection.onSignatureHelp((params: TextDocumentPositionParams) => {
 
     // add all matching signatures to the results array
     for (const signa of tspItem.commandSet.signatures) {
-        const signaBeforeParams: string = signa.label.slice(0, signa.labal.indexOf('('))
+        const signaBeforeParams: string = signa.label.slice(0, signa.label.indexOf('('))
 
         if (signaBeforeParams.localeCompare(unreversed) === 0) {
             results.push(signa)