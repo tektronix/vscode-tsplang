3e87dd802ef34a91c5105792cf6df658fbc63de0
Evan Hall <evan.tom.hall@gmail.com>
2018-09-22 12:50:46 -0400

    Fixed ContentProcessor based on unit tests

--- server/src/contentProcessor.ts
+++ server/src/contentProcessor.ts
@@ -65,40 +65,45 @@ export const doubleSquareBrackets: Pair = {
 }
 
 export const getOffsetOfUnmatched = (text: string, type: Pair, reverse: boolean): number | undefined => {
+    const offset = (reverse) ? text.length - 1 : 0
     let state: Next = {
-        char: '',
-        offset: (reverse) ? text.length - 1 : 0
+        offset,
+        char: text.charAt(offset)
     }
 
     do {
         let foundType: Pair | undefined
 
-        state = next(text, state.offset, reverse)
+        const index1 = (reverse) ? 1 : 0
 
         if (state.char.localeCompare(type.close(reverse)) === 0) {
             return state.offset
         }
-        else if (state.char.localeCompare(singleQuotes.open(reverse)) === 0) {
-            foundType = singleQuotes
-            state.offset = consumePair(text, state.offset, singleQuotes, reverse)
-        }
         else if (state.char.localeCompare(doubleQuotes.open(reverse)) === 0) {
             foundType = doubleQuotes
-            state.offset = consumePair(text, state.offset, doubleQuotes, reverse)
         }
-        else if (state.char.localeCompare(doubleSquareBrackets.open(reverse)[0]) === 0) {
-            // lookahead check to see if the next character is also an open square bracket
-            if (peek(text, state.offset, reverse).localeCompare(doubleSquareBrackets.open(reverse)[0]) === 0) {
+        else if (state.char.localeCompare(doubleSquareBrackets.open(reverse)[index1]) === 0) {
+            const index2 = (reverse) ? 0 : 1
+
+            // Lookahead check to see if the next character is also an open square bracket.
+            if (peek(text, state.offset, reverse).localeCompare(doubleSquareBrackets.open(reverse)[index2]) === 0) {
                 foundType = doubleSquareBrackets
-                state = next(text, state.offset, reverse)
             }
+        }
+        else if (state.char.localeCompare(singleQuotes.open(reverse)) === 0) {
+            foundType = singleQuotes
+        }
+        else if (state.char.localeCompare(parentheses.open(reverse)) === 0) {
+            foundType = parentheses
         }
 
         if (foundType !== undefined) {
             state.offset = consumePair(text, state.offset, foundType, reverse)
         }
 
-    } while (state.char.localeCompare('') === 0)
+        state = next(text, state.offset, reverse)
+
+    } while (state.char.localeCompare('') !== 0)
 
     return
 }
@@ -106,25 +111,85 @@ export const getOffsetOfUnmatched = (text: string, type: Pair, reverse: boolean)
 /**
  * @returns The new offset.
  */
-const consumePair = (text: string, offset: number, type: Pair, reverse: boolean): number => {
+function consumePair(text: string, offset: number, type: Pair, reverse: boolean): number {
     let lastCharWasEscape = false
     let state: Next = {
         offset,
-        char: ''
+        char: text.charAt(offset)
     }
 
     do {
-        state = next(text, state.offset, false)
 
-        if (state.char.localeCompare(type.close(reverse)) === 0 && !lastCharWasEscape) {
-            return state.offset
+        state = next(text, state.offset, reverse)
+
+        if (reverse && type.escape !== undefined) {
+            lastCharWasEscape = (peek(text, state.offset, reverse).localeCompare(type.escape) === 0)
+        }
+
+        const index1 = (reverse && type.close(reverse).length > 1) ? 1 : 0
+        const index2 = (reverse) ? 0 : 1
+
+        if (state.char.localeCompare(type.close(reverse)[index1]) === 0 && !lastCharWasEscape) {
+            // If the closing string is more than 1 character.
+            if (type.close(reverse).length > 1) {
+                // Lookahead check to see if the next character also matches the closing character.
+                if (peek(text, state.offset, reverse).localeCompare(type.close(reverse)[index2]) === 0) {
+                    state = next(text, state.offset, reverse)
+
+                    return state.offset
+                }
+            }
+            else {
+                return state.offset
+            }
+        }
+
+        let recurseType: Pair | undefined
+
+        if (state.char.localeCompare(doubleQuotes.open(reverse)) === 0) {
+            // If we are not inside double square brackets and we are not inside single quotes.
+            if (type.open(reverse).localeCompare(doubleSquareBrackets.open(reverse)) !== 0
+                && type.open(reverse).localeCompare(singleQuotes.open(reverse)) !== 0) {
+                recurseType = doubleQuotes
+            }
+        }
+        else if (state.char.localeCompare(doubleSquareBrackets.open(reverse)[index1]) === 0) {
+            if (peek(text, state.offset, reverse).localeCompare(doubleSquareBrackets.open(reverse)[index2]) === 0) {
+                // If we are not inside double quotes and we are not inside single quotes.
+                if (type.open(reverse).localeCompare(doubleQuotes.open(reverse)) !== 0
+                    && type.open(reverse).localeCompare(singleQuotes.open(reverse)) !== 0) {
+                    recurseType = doubleSquareBrackets
+                }
+            }
+        }
+        else if (state.char.localeCompare(parentheses.open(reverse)) === 0) {
+            // If we are not inside double quotes,
+            // and we are not inside double square brackets,
+            // and we are not inside single quotes.
+            if (type.open(reverse).localeCompare(doubleQuotes.open(reverse)) !== 0
+                && type.open(reverse).localeCompare(doubleSquareBrackets.open(reverse)) !== 0
+                && type.open(reverse).localeCompare(singleQuotes.open(reverse)) !== 0) {
+                recurseType = parentheses
+            }
+        }
+        else if (state.char.localeCompare(singleQuotes.open(reverse)) === 0) {
+            // If we are not inside double quotes and we are not inside double square brackets
+            if (type.open(reverse).localeCompare(doubleQuotes.open(reverse)) !== 0
+                && type.open(reverse).localeCompare(doubleSquareBrackets.open(reverse)) !== 0) {
+                recurseType = singleQuotes
+            }
+        }
+
+        if (recurseType !== undefined && !lastCharWasEscape) {
+            state.offset = consumePair(text, state.offset, recurseType, reverse)
+            state.char = text.charAt(state.offset)
         }
 
         if (type.escape !== undefined) {
             lastCharWasEscape = (state.char.localeCompare(type.escape) === 0)
         }
 
-    } while (state.char.localeCompare('') === 0)
+    } while (state.char.localeCompare('') !== 0)
 
     return offset
 }
