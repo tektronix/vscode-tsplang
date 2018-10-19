032f915461e60567f7b8bf16cd30a5ee3451995a
Evan Hall <evan.hall@keithley.com>
2018-10-18 20:15:25 -0400

    Renamed assignments.lua test file

--- test/grammars/assignments.lua
+++
@@ -1,9 +0,0 @@
-a = 'local,.' b, c = 2, 3
-local d = 4
-local e, f = 5, 6
-b, c = 1 == 2, nil
-g = {}
-g.h.x['y'] = 8
-g['h'] = 8
-i = display.BUTTONS_OK
-j = function() end
---
+++ test/grammars/bit-of-everything.lua
@@ -0,0 +1,19 @@
+a = 'local,.' b, c = 2, 3
+function func() return end
+local function lclfunc() end
+local d = 4
+local e, f = 5, 6
+b, c = 1 == 2, nil
+g = {}
+g = { nil }
+g = { h = 1, q = 2; o = 3 }
+(g).h = 1
+g.h.x['y'] = 8
+g['h'] = 8
+g().a = 1
+i = display.BUTTONS_OK
+j = function(a,b) end
+k = l'blah'
+k = l('blah')
+l('blah')
+l()
