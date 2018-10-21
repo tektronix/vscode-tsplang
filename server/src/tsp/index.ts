e117ea211e8043f3b8777e672e92bfe768a899ad
Evan Hall <evan.tom.hall@gmail.com>
2018-10-21 14:49:59 -0400

    Re-enabled tslint rules

--- server/src/tsp/index.ts
+++ server/src/tsp/index.ts
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-/* tslint:disable: no-var-requires no-require-imports */
 'use strict'
 
 export { TspLexer } from './TspLexer'
