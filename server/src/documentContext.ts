340bc4d68892958f63131e40c4a8d73d5428c246
Evan Hall <evan.hall@keithley.com>
2018-10-24 18:48:08 -0400

    Added unused name parsing functions

--- server/src/documentContext.ts
+++ server/src/documentContext.ts
@@ -17,7 +17,7 @@
 
 import { CommonTokenStream, InputStream, ParserRuleContext } from 'antlr4'
 // tslint:disable-next-line:no-submodule-imports
-import { ParseTreeWalker } from 'antlr4/tree/Tree'
+import { ParseTreeWalker, TerminalNode } from 'antlr4/tree/Tree'
 import { CompletionItem, CompletionItemKind } from 'vscode-languageserver'
 
 import { TspLexer, TspListener, TspParser } from './tsp'
@@ -102,16 +102,121 @@ export class DocumentContext extends TspListener {
         ParseTreeWalker.DEFAULT.walk(this, this.parseTree)
     }
 
+    private getCompletionsFromContext = (context: ParserRuleContext): Array<CompletionItem> => {
+        const result = new Array<CompletionItem>()
+
+        const getCompletionsRecursive = (node: ParserRuleContext | TerminalNode): void => {
+            if (node instanceof TerminalNode && node.symbol.type === TspLexer.NAME) {
+                result.push({
+                    kind: CompletionItemKind.Variable,
+                    label: node.symbol.text
+                })
+            }
+
+            for (let i = 0; i < node.getChildCount(); i++) {
+                const child = node.getChild(i)
+
+                if (child === null) {
+                    continue
+                }
+
+                if (child instanceof TspParser.SuffixContext) {
+
+                    continue
+                }
+
+                getCompletionsRecursive(child)
+            }
+        }
+        getCompletionsRecursive(context)
+
+        return result
+    }
+
     private getGlobalCompletionItems(): Array<CompletionItem> {
         return Array.from(this.globals.values())
     }
 
-    // private isGlobalFunction = (context: TspParser.StatContext | TspParser.AssignmentContext): boolean => {
-    //     if (context instanceof TspParser.StatContext) {
-    //         return (context.funcname() !== null && context.funcbody() !== null)
-    //     }
-    //     else {
-    //         // Perform a local lookup to see if the variable is listed
-    //     }
-    // }
+    private getPrefixSuffixCompletions = (context: TspParser.VariableContext): Array<CompletionItem> => {
+        const result = new Array<CompletionItem>()
+
+        const prefix = context.prefix()
+        if (prefix === null) {
+            return []
+        }
+
+        const prefixName = prefix.NAME()
+        if (prefixName === null) {
+            return []
+        }
+
+        result.push({
+            kind: CompletionItemKind.Module,
+            label: prefixName.symbol.text
+        })
+
+        let i = -1
+        const suffix = context.suffix()
+        for (const suffixItem of suffix) {
+            // Located at the start of the loop so it do not have to appear before every loop
+            // control keyword.
+            i++
+
+            // result is guaranteed to have an item
+            const lastItem = result.pop()
+
+            if (lastItem === undefined) {
+                break
+            }
+
+            // Create this suffix's namespace
+            const data = new Array<string>(lastItem.label)
+            if (lastItem.data !== undefined) {
+                data.push(...lastItem.data)
+            }
+
+            const objectCall = suffixItem.objectCall()
+            if (objectCall !== null) {
+                const callName = objectCall.NAME()
+                if (callName !== null) {
+                    result.push(lastItem)
+                    result.push({
+                        data,
+                        kind: CompletionItemKind.Function,
+                        label: callName.symbol.text
+                    })
+                }
+                else {
+                    lastItem.kind = CompletionItemKind.Function
+                    result.push(lastItem)
+                }
+
+                // Do not process any more suffixes/indices.
+                break
+            }
+
+            result.push(lastItem)
+
+            const index = suffixItem.index()
+            if (index === null) {
+                continue
+            }
+
+            const indexName = index.NAME()
+            if (indexName === null) {
+                continue
+            }
+
+            result.push({
+                data,
+                // If this is not the last suffix then it is a table.
+                kind: (i !== (suffix.length - 1)) ?
+                    CompletionItemKind.Module :
+                    CompletionItemKind.Variable,
+                label: indexName.symbol.text
+            })
+        }
+
+        return result
+    }
 }
