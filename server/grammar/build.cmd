7d69a7c00712146c4e81f9a18876dd77c55be6f5
Evan Hall <evan.hall@keithley.com>
2018-11-08 11:42:49 -0500

    Added direct TS output to ANTLR4 build.cmd
    Currently disabled.

--- server/grammar/build.cmd
+++ server/grammar/build.cmd
@@ -1,1 +1,8 @@
-java org.antlr.v4.Tool -o %~dp0../src/tsp -Dlanguage=JavaScript %~dp0Tsp.g4
+@echo off
+java org.antlr.v4.Tool -o %~dp0..\src\tsp -Dlanguage=JavaScript %~dp0Tsp.g4
+
+rem for %%f in (%~dp0..\src\tsp\*.js) do (
+rem     echo // tslint:disable>%%~dpnf.ts
+rem     type %%f >> %%~dpnf.ts
+rem     del %%f
+rem )
