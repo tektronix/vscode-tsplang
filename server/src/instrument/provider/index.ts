479441a6962ffc29e2b7b8324344868d298d4a45
Evan Hall <evan.tom.hall@gmail.com>
2018-11-25 14:02:38 -0500

    Added 'use strict'

--- server/src/instrument/provider/index.ts
+++ server/src/instrument/provider/index.ts
@@ -13,6 +13,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+'use strict'
+
 // tslint:disable-next-line:no-require-imports
 import escapeStringRegexp = require('escape-string-regexp')
 
