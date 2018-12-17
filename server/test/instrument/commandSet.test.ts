0524586384eed75274e940290d95f990a0e0abcb
Evan Hall <evan.tom.hall@gmail.com>
2018-12-17 00:00:18 -0500

    Added unit tests for CommandSet

--- server/test/instrument/commandSet.test.ts
+++ server/test/instrument/commandSet.test.ts
@@ -19,89 +19,140 @@ import { expect } from 'chai'
 import 'mocha'
 // tslint:enable:no-implicit-dependencies
 
-import { CompletionItem, SignatureInformation } from '../../src/decorators'
-import { CommandSetInterface } from '../../src/instrument'
+import { CompletionItem, MarkupContent, MarkupContentCallback, SignatureInformation } from '../../src/decorators'
+import { CommandSet, CommandSetInterface } from '../../src/instrument'
+
+import { emptySpec } from '../instrument/emptySpec'
+
+import { ArrayUnion, MapMerge } from './helpers'
 
 describe('Instrument', () => {
+    const emptyDocCallback: MarkupContentCallback = (): MarkupContent => MarkupContent``
+
+    const rootADoc: [string, MarkupContentCallback] = [
+        'root',
+        emptyDocCallback
+    ]
+    const rootAFooDoc: [string, MarkupContentCallback] = [
+        'root.foo',
+        emptyDocCallback
+    ]
+    const rootABarDoc: [string, MarkupContentCallback] = [
+        'foo.bar',
+        emptyDocCallback
+    ]
+    const rootASubrootDoc: [string, MarkupContentCallback] = [
+        'root.subroot',
+        emptyDocCallback
+    ]
+    const rootASubrootFooDoc: [string, MarkupContentCallback] = [
+        'root.subroot.foo',
+        emptyDocCallback
+    ]
+    const rootASubrootBarDoc: [string, MarkupContentCallback] = [
+        'root.subroot.bar',
+        emptyDocCallback
+    ]
+    const rootASubrootBazDoc: [string, MarkupContentCallback] = [
+        'root.subroot.baz',
+        emptyDocCallback
+    ]
+
+    const rootACompl: CompletionItem = {
+        label: 'root'
+    }
+    const rootAFooCompl: CompletionItem = {
+        data: { domains: ['root'] },
+        label: 'foo'
+    }
+    const rootABarCompl: CompletionItem = {
+        data: { domains: ['root'] },
+        label: 'bar'
+    }
+    const rootASubrootCompl: CompletionItem = {
+        data: { domains: ['root'] },
+        label: 'subroot'
+    }
+    const rootASubrootFooCompl: CompletionItem = {
+        data: { domains: ['subroot', 'root'] },
+        label: 'foo'
+    }
+    const rootASubrootBarCompl: CompletionItem = {
+        data: { domains: ['subroot', 'root'] },
+        label: 'bar'
+    }
+    const rootASubrootBazCompl: CompletionItem = {
+        data: { domains: ['subroot', 'root'] },
+        label: 'baz'
+    }
+
+    const rootAFooSig: SignatureInformation = {
+        label: 'root.foo()'
+    }
+    const rootABarSig: SignatureInformation = {
+        label: 'root.bar()'
+    }
+    const rootASubrootFooSig: SignatureInformation = {
+        label: 'root.subroot[].foo()'
+    }
+    const rootASubrootBarSig: SignatureInformation = {
+        label: 'root.subroot[].bar()'
+    }
+    const rootASubrootBazSig: SignatureInformation = {
+        label: 'root.subroot[].baz()'
+    }
+
+    const documentationSetA: Map<string, MarkupContentCallback> = new Map([
+        rootADoc,
+        rootAFooDoc,
+        rootASubrootDoc,
+        rootASubrootFooDoc
+    ])
+    const documentationSetB: Map<string, MarkupContentCallback> = new Map([
+        rootADoc,
+        rootAFooDoc,
+        rootABarDoc,
+        rootASubrootDoc,
+        rootASubrootFooDoc,
+        rootASubrootBarDoc,
+        rootASubrootBazDoc
+    ])
+
+    const completionSetA: Array<CompletionItem> = [
+        rootACompl,
+        rootAFooCompl,
+        rootABarCompl,
+        rootASubrootCompl,
+        rootASubrootFooCompl,
+        rootASubrootBarCompl,
+        rootASubrootBazCompl
+    ]
+    const completionSetB: Array<CompletionItem> = [
+        rootACompl,
+        rootACompl,
+        rootACompl,
+        rootASubrootBarCompl,
+        rootASubrootBarCompl,
+        rootAFooCompl
+    ]
+
+    const signatureSetA: Array<SignatureInformation> = [
+        rootAFooSig,
+        rootABarSig,
+        rootASubrootFooSig,
+        rootASubrootBarSig,
+        rootASubrootBazSig
+    ]
+    const signatureSetB: Array<SignatureInformation> = [
+        rootAFooSig,
+        rootAFooSig,
+        rootAFooSig,
+        rootASubrootBarSig,
+        rootASubrootBarSig,
+        rootASubrootFooSig,
+    ]
+
     describe('CommandSetInterface', () => {
-        const rootACompl: CompletionItem = {
-            label: 'root'
-        }
-        const rootAFooCompl: CompletionItem = {
-            data: { domains: ['root'] },
-            label: 'foo'
-        }
-        const rootABarCompl: CompletionItem = {
-            data: { domains: ['root'] },
-            label: 'bar'
-        }
-        const rootASubrootCompl: CompletionItem = {
-            data: { domains: ['root'] },
-            label: 'subroot'
-        }
-        const rootASubrootFooCompl: CompletionItem = {
-            data: { domains: ['subroot', 'root'] },
-            label: 'foo'
-        }
-        const rootASubrootBarCompl: CompletionItem = {
-            data: { domains: ['subroot', 'root'] },
-            label: 'bar'
-        }
-        const rootASubrootBazCompl: CompletionItem = {
-            data: { domains: ['subroot', 'root'] },
-            label: 'baz'
-        }
-
-        const rootAFooSig: SignatureInformation = {
-            label: 'root.foo()'
-        }
-        const rootABarSig: SignatureInformation = {
-            label: 'root.bar()'
-        }
-        const rootASubrootFooSig: SignatureInformation = {
-            label: 'root.subroot[].foo()'
-        }
-        const rootASubrootBarSig: SignatureInformation = {
-            label: 'root.subroot[].bar()'
-        }
-        const rootASubrootBazSig: SignatureInformation = {
-            label: 'root.subroot[].baz()'
-        }
-
-        const completionSetA: Array<CompletionItem> = [
-            rootACompl,
-            rootAFooCompl,
-            rootABarCompl,
-            rootASubrootCompl,
-            rootASubrootFooCompl,
-            rootASubrootBarCompl,
-            rootASubrootBazCompl
-        ]
-        const completionSetB: Array<CompletionItem> = [
-            rootACompl,
-            rootACompl,
-            rootACompl,
-            rootASubrootBarCompl,
-            rootASubrootBarCompl,
-            rootAFooCompl
-        ]
-
-        const signatureSetA: Array<SignatureInformation> = [
-            rootAFooSig,
-            rootABarSig,
-            rootASubrootFooSig,
-            rootASubrootBarSig,
-            rootASubrootBazSig
-        ]
-        const signatureSetB: Array<SignatureInformation> = [
-            rootAFooSig,
-            rootAFooSig,
-            rootAFooSig,
-            rootASubrootBarSig,
-            rootASubrootBarSig,
-            rootASubrootFooSig,
-        ]
-
         describe('.getCompletionMap()', () => {
             const expectedMapA = new Map<string, Array<CompletionItem>>([
                 ['root', [rootACompl]],
@@ -156,8 +207,148 @@ describe('Instrument', () => {
     })
 
     describe('CommandSet', () => {
+        let commandSet: CommandSet
+
+        before(() => {
+            commandSet = new CommandSet(emptySpec)
+        })
+
+        describe('#completionDocs', () => {
+            it('is empty on instantiation', () => {
+                expect(commandSet.completionDocs).to.be.empty
+            })
+        })
+
+        describe('#completions', () => {
+            it('is empty on instantiation', () => {
+                expect(commandSet.completions).to.be.empty
+            })
+        })
+
+        describe('#signatures', () => {
+            it('is empty on instantiation', () => {
+                expect(commandSet.signatures).to.be.empty
+            })
+        })
+
+        describe('#specification', () => {
+            it('contains the passed InstrumentSpec', () => {
+                expect(commandSet.specification).to.deep.equal(emptySpec)
+            })
+        })
+
         describe('#add()', () => {
-            // expect.fail()
+            const emptyCommandSetInterface: CommandSetInterface = {
+                completions: []
+            }
+
+            it('does not add completion documentation when undefined', () => {
+                expect(commandSet.completionDocs).to.be.empty
+
+                commandSet.add(emptyCommandSetInterface)
+
+                expect(commandSet.completionDocs).to.be.empty
+            })
+
+            it('does not add completions when none are given', () => {
+                expect(commandSet.completions).to.be.empty
+
+                commandSet.add(emptyCommandSetInterface)
+
+                expect(commandSet.completions).to.be.empty
+            })
+
+            it('does not add signatures when undefined', () => {
+                expect(commandSet.signatures).to.be.empty
+
+                commandSet.add(emptyCommandSetInterface)
+
+                expect(commandSet.signatures).to.be.empty
+            })
+
+            it('adds completion documentation to the empty set', () => {
+                expect(commandSet.completionDocs).to.be.empty
+
+                commandSet.add({
+                    completionDocs: documentationSetA,
+                    completions: []
+                })
+
+                expect(commandSet.completionDocs).to.deep.equal(documentationSetA)
+            })
+
+            it('adds completions to the empty set', () => {
+                expect(commandSet.completions).to.be.empty
+
+                commandSet.add({
+                    completions: completionSetA
+                })
+
+                expect(commandSet.completions).to.deep.equal(completionSetA)
+            })
+
+            it('adds signatures to the empty set', () => {
+                expect(commandSet.signatures).to.be.empty
+
+                commandSet.add({
+                    completions: [],
+                    signatures: signatureSetA
+                })
+
+                expect(commandSet.signatures).to.deep.equal(signatureSetA)
+            })
+
+            it('merges completion documentation into an existing set', () => {
+                if (commandSet.completionDocs.size === 0) {
+                    commandSet.add({
+                        completionDocs: documentationSetA,
+                        completions: []
+                    })
+                }
+
+                expect(commandSet.completionDocs).to.deep.equal(documentationSetA)
+
+                commandSet.add({
+                    completionDocs: documentationSetB,
+                    completions: []
+                })
+
+                expect(commandSet.completionDocs).to.deep.equal(MapMerge(documentationSetA, documentationSetB))
+            })
+
+            it('merges completions into an existing set', () => {
+                if (commandSet.completions.length === 0) {
+                    commandSet.add({
+                        completions: completionSetA
+                    })
+                }
+
+                expect(commandSet.completions).to.deep.equal(completionSetA)
+
+                commandSet.add({
+                    completions: completionSetB
+                })
+
+                expect(commandSet.completions).to.deep.equal(ArrayUnion(completionSetA, completionSetB))
+            })
+
+            it('merges signatures into an existing set', () => {
+                if (commandSet.signatures.length === 0) {
+                    commandSet.add({
+                        completions: [],
+                        signatures: signatureSetA
+                    })
+                }
+
+                expect(commandSet.signatures).to.deep.equal(signatureSetA)
+
+                commandSet.add({
+                    completions: [],
+                    signatures: signatureSetB
+                })
+
+                expect(commandSet.signatures).to.deep.equal(ArrayUnion(signatureSetA, signatureSetB))
+            })
         })
     })
 })
