cb9c96ddba30e148f11d104be221cfd52b34d873
Evan Hall <evan.tom.hall@gmail.com>
2018-12-22 15:14:50 -0500

    Tests now included in compilation

---
+++ server/src/test/instrument/lua/index.test.ts
@@ -0,0 +1,83 @@
+/*
+ *  Copyright 2018 Tektronix Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+// tslint:disable:no-implicit-dependencies no-unused-expression
+import { expect } from 'chai'
+// tslint:disable-next-line:no-import-side-effect
+import 'mocha'
+// tslint:enable:no-implicit-dependencies
+
+import { ApiSpec, InstrumentModule, InstrumentSpec } from '../../../instrument'
+import { emptySpecUndefinedOptionals } from '../emptySpec'
+
+describe('Instrument Specification', () => {
+    describe('Lua', () => {
+        let instrumentModule: InstrumentModule
+
+        before(() => {
+            // tslint:disable-next-line:no-require-imports
+            instrumentModule = require('../../../src/instrument/lua')
+        })
+
+        it('exports "getApiSpec"', () => {
+            expect(instrumentModule).to.haveOwnProperty('getApiSpec')
+        })
+
+        it('exports "getInstrumentSpec"', () => {
+            expect(instrumentModule).to.haveOwnProperty('getInstrumentSpec')
+        })
+
+        describe('getApiSpec()', () => {
+            const knownNamespaces = [
+                'coroutine',
+                'functions',
+                'keywords',
+                'math',
+                'os',
+                'string',
+                'table'
+            ]
+            let specs: Array<ApiSpec>
+
+            before(() => {
+                specs = instrumentModule.getApiSpec()
+            })
+
+            it('contains all known namespaces', () => {
+                knownNamespaces.forEach((namespace: string) => {
+                    const result = specs.some((spec: ApiSpec) => spec.label.localeCompare(namespace) === 0)
+
+                    expect(result, `failed to contain the "${namespace}" namespace`).to.be.true
+                })
+            })
+
+            it('contains no additional namespaces', () => {
+                expect(specs.length).to.equal(knownNamespaces.length)
+            })
+        })
+
+        describe('getInstrumentSpec()', () => {
+            let spec: InstrumentSpec
+
+            before(() => {
+                spec = instrumentModule.getInstrumentSpec()
+            })
+
+            it('is empty', () => {
+                expect(spec).to.deep.equal(emptySpecUndefinedOptionals)
+            })
+        })
+    })
+})
---
+++ server/src/test/model.test.ts
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2018 Tektronix Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+// tslint:disable:no-implicit-dependencies no-unused-expression
+import { expect } from 'chai'
+// tslint:disable-next-line:no-import-side-effect
+import 'mocha'
+// tslint:enable:no-implicit-dependencies
+
+import { Model } from '../model'
+
+describe('Model', () => {
+    describe('.KI2450', () => {
+        it('contained in Model', () => {
+            expect(Model).to.have.ownProperty('KI2450')
+        })
+
+        it('equals "2450"', () => {
+            expect(Model.KI2450).to.equal('2450')
+        })
+    })
+
+    describe('.KI2460', () => {
+        it('contained in Model', () => {
+            expect(Model).to.have.ownProperty('KI2460')
+        })
+
+        it('equals "2460"', () => {
+            expect(Model.KI2460).to.equal('2460')
+        })
+    })
+
+    describe('.LUA', () => {
+        it('contained in Model', () => {
+            expect(Model).to.have.ownProperty('LUA')
+        })
+
+        it('equals "lua"', () => {
+            expect(Model.LUA).to.equal('lua')
+        })
+    })
+
+    describe('.fromString', () => {
+        it('returns a Model type when passed "2450"', () => {
+            const result = Model.fromString('2450')
+
+            expect(result).to.equal(Model.KI2450)
+        })
+
+        it('returns a Model type when passed "2460"', () => {
+            const result = Model.fromString('2460')
+
+            expect(result).to.equal(Model.KI2460)
+        })
+
+        it('returns a Model type when passed "lua"', () => {
+            const result = Model.fromString('lua')
+
+            expect(result).to.equal(Model.LUA)
+        })
+
+        it('returns undefined when passed an unsupported model', () => {
+            const result = Model.fromString('unsupported')
+
+            expect(result).to.be.undefined
+        })
+    })
+})
