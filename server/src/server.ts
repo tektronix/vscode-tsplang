090727d953ce8e42543c27f23eebd9cd86406470
Evan Hall <evan.tom.hall@gmail.com>
2019-01-12 14:57:19 -0500

    Move server callbacks to ServerContext

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,9 +15,9 @@
  */
 'use strict'
 
-import { CompletionItem, createConnection, DidChangeConfigurationNotification, DidChangeConfigurationParams, IConnection, InitializeParams, InitializeResult, IPCMessageReader, IPCMessageWriter, SignatureHelp, TextDocument, TextDocumentChangeEvent, TextDocumentPositionParams, TextDocuments } from 'vscode-languageserver'
+import { CompletionItem, createConnection, DidChangeConfigurationParams, IConnection, InitializeParams, InitializeResult, IPCMessageReader, IPCMessageWriter, SignatureHelp, TextDocumentChangeEvent, TextDocumentPositionParams, TextDocuments } from 'vscode-languageserver'
 
-import { TsplangSettings } from './settings'
+import { ServerContext } from './serverContext'
 import { TspManager } from './tspManager'
 
 // Create a connection for the server. The connection uses Node's IPC as a transport
@@ -33,146 +33,44 @@ const documents: TextDocuments = new TextDocuments()
 // Create a TSP Manager to provide command set completions.
 const manager: TspManager = new TspManager(documents)
 
-// We need the last document URI to request a completion to prevent a bunch of
-// expensive completion lookups.
-let lastCompletionUri: string | undefined
-
-let hasWorkspaceSettings = false
-let globalSettings: TsplangSettings = TsplangSettings.defaults()
+const context = new ServerContext()
 
 // After the server has started the client sends an initialize request. The server receives in the
 // passed params the rootPath of the workspace plus the client capabilities.
 connection.onInitialize((params: InitializeParams): InitializeResult => {
-    console.log('tsplang connection initialized')
-
-    hasWorkspaceSettings = !!(params.capabilities.workspace && !!params.capabilities.workspace.configuration)
-
-    return {
-        capabilities: {
-            // provide context sensitive suggestions to the user
-            completionProvider: {
-                resolveProvider: true,
-                triggerCharacters: ['.']
-            },
-            // display information about the function/method that is being called
-            signatureHelpProvider: {
-                triggerCharacters: [ ',', '(' ]
-            },
-            // Tell the client that the server works in FULL text document sync mode
-            textDocumentSync: documents.syncKind
-        }
-    }
+    return context.onInitialize(params, documents)
 })
 
 connection.onInitialized(() => {
-    if (hasWorkspaceSettings) {
-        connection.client.register(DidChangeConfigurationNotification.type, { section: 'tsplang' })
-    }
+    context.onInitialized(connection)
 })
 
 // The content of a text document has changed. This event is emitted when the text document first
 // opened or when its content has changed.
 documents.onDidChangeContent((change: TextDocumentChangeEvent) => {
-    // if document is registered, then update
-    if (manager.has(change.document.uri)) {
-        const diagnostics = manager.update(change.document.uri)
-
-        if (diagnostics !== undefined) {
-            connection.sendDiagnostics({ diagnostics, uri: change.document.uri })
-        }
-    }
-    // if document is unregistered, then register
-    else {
-        // Register this document with global settings.
-        let settings = globalSettings
-
-        // Override global settings with any workspace settings.
-        if (hasWorkspaceSettings) {
-            connection.workspace.getConfiguration({
-                scopeUri: change.document.uri,
-                section: 'tsplang'
-            })
-            .then(
-                (value: TsplangSettings) => {
-                    settings = value
-                },
-                (reason: Error) => { return }
-            )
-        }
-
-        const diagnostics = manager.register(change.document.uri, settings) || []
-
-        connection.sendDiagnostics({ diagnostics, uri: change.document.uri })
-    }
+    context.onDidChangeContent(change, connection, manager)
 })
 
 documents.onDidClose((params: TextDocumentChangeEvent) => {
-    connection.console.log(`${params.document.uri} closed.`)
-
-    if (manager.has(params.document.uri)) {
-        manager.unregister(params.document.uri)
-
-        connection.sendDiagnostics({ diagnostics: [], uri: params.document.uri })
-    }
+    context.onDidClose(params, connection, manager)
 })
 
 // This handler provides the initial list of completion items.
 connection.onCompletion((params: TextDocumentPositionParams): Array<CompletionItem> | undefined => {
-    lastCompletionUri = params.textDocument.uri
-
-    const tspItem = manager.get(params.textDocument.uri)
-
-    if (tspItem === undefined) {
-        return
-    }
-
-    return tspItem.context.getCompletionItems(params.position)
+    return context.onCompletion(params, manager)
 })
 
 // This handler resolves additional information for the item selected in the completion list.
 connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-    if (lastCompletionUri === undefined) {
-        return item
-    }
-
-    const tspItem = manager.get(lastCompletionUri)
-
-    if (tspItem === undefined) {
-        return item
-    }
-
-    return tspItem.context.resolveCompletion(item)
+    return context.onCompletionResolve(item, manager)
 })
 
 connection.onDidChangeConfiguration((params: DidChangeConfigurationParams) => {
-    if (hasWorkspaceSettings) {
-        // Update all open document contexts.
-        documents.all().forEach(async (document: TextDocument) => {
-            const settings = await connection.workspace.getConfiguration({
-                scopeUri: document.uri,
-                section: 'tsplang'
-            })
-
-            const tspItem = manager.get(document.uri)
-
-            if (tspItem !== undefined) {
-                tspItem.context.settings = settings
-            }
-        })
-    }
-    else {
-        globalSettings = params.settings.tsplang || TsplangSettings.defaults()
-    }
+    context.onDidChangeConfiguration(params, connection, documents, manager)
 })
 
 connection.onSignatureHelp((params: TextDocumentPositionParams): SignatureHelp | undefined => {
-    const tspItem = manager.get(params.textDocument.uri)
-
-    if (tspItem === undefined) {
-        return
-    }
-
-    return tspItem.context.getSignatureHelp(params.position)
+    return context.onSignatureHelp(params, manager)
 })
 
 // Make the text document manager listen on the connection for open, change and close text
