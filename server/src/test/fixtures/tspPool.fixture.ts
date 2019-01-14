892f31eb83371c14cbd537a1af2cb4601f06f4cd
Evan Hall <evan.tom.hall@gmail.com>
2019-01-14 00:07:12 -0500

    Added TspPool unit tests

---
+++ server/src/test/fixtures/tspPool.fixture.ts
@@ -0,0 +1,37 @@
+/*
+ *  Copyright Tektronix Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { Model } from '../../model'
+import { PoolEntry, TspPool } from '../../tspPool'
+
+declare module '../../tspPool' {
+    export interface TspPool {
+        _get(model: Model): PoolEntry
+        _load(model: Model): PoolEntry
+        delete(model: Model): boolean
+    }
+}
+TspPool.prototype._get = function(model: Model): PoolEntry {
+    // tslint:disable-next-line:no-invalid-this
+    return this.get(model)
+}
+TspPool.prototype._load = function(model: Model): PoolEntry {
+    // tslint:disable-next-line:no-invalid-this
+    return this.load(model)
+}
+TspPool.prototype.delete = function(model: Model): boolean {
+    // tslint:disable-next-line:no-invalid-this
+    return this.pool.delete(model)
+}
--- server/src/test/tspPool.test.ts
+++ server/src/test/tspPool.test.ts
@@ -13,14 +13,35 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-// tslint:disable:no-implicit-dependencies no-unused-expression
+// tslint:disable:no-implicit-dependencies no-import-side-effect no-unused-expression
 import { expect } from 'chai'
-// tslint:disable-next-line:no-import-side-effect
 import 'mocha'
 // tslint:enable:no-implicit-dependencies
 
+import { Model } from '../model'
 import { TspPool } from '../tspPool'
 
+import './fixtures/model.fixture'
+import './fixtures/tspPool.fixture'
+
 describe('TspPool', () => {
-    it.skip('needs test coverage')
+    const pool = new TspPool()
+
+    describe('#get()', () => {
+        it('throws an error if TspPool does not contain the Model', () => {
+            const model = Model.KI2450
+
+            expect(() => { pool._get(model) })
+                .to.throw(`attempted to access the non-existant ${model} entry`)
+        })
+    })
+
+    describe('#load()', () => {
+        it('throws an error if passed an unsupported Model', () => {
+            const model = Model.UNSUPPORTED
+
+            expect(() => { pool._load(model as Model) })
+                .to.throw(`model ${model} is not supported`)
+        })
+    })
 })
