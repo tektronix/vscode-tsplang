ece01b88c0574be4a450bd76b0d8ce64a29c4b2d
Evan Hall <evan.hall@keithley.com>
2020-01-07 16:35:59 -0500

    Created a dedicated emitter for the plugin provider

---
+++ server/@tsplang-plugins/src/pluginProviderEmitter.ts
@@ -0,0 +1,96 @@
+/*
+ *  Copyright Tektronix Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+import { EventEmitter } from "events"
+
+import { ErrorObject } from "ajv"
+
+import { Disposable } from "./disposable"
+
+const CONFIG_READ_ERROR = "ConfigReadError"
+const CONFLICTING_NAME_ERROR = "ConflictingNameError"
+const EXTENDS_UNKNOWN_PLUGIN_ERROR = "ExtendsUnknownPluginError"
+const INVALID_CONFIG_ERROR = "InvalidConfigError"
+
+const ConfigReadError = function(reason: Error): void {
+    ;(this.emitter as EventEmitter).emit(CONFIG_READ_ERROR, reason)
+}
+const ConflictingNameError = function(
+    existingPlugin: string,
+    thisPlugin: string,
+    conflictingName: string,
+    alias: boolean
+): void {
+    ;(this.emitter as EventEmitter).emit(
+        CONFLICTING_NAME_ERROR,
+        existingPlugin,
+        thisPlugin,
+        conflictingName,
+        alias
+    )
+}
+const ExtendsUnknownPluginError = function(
+    thisPlugin: string,
+    unknownPlugin: string
+): void {
+    ;(this.emitter as EventEmitter).emit(
+        EXTENDS_UNKNOWN_PLUGIN_ERROR,
+        thisPlugin,
+        unknownPlugin
+    )
+}
+/**
+ * If `null`, then validation of the plugin config failed but AJV provided
+ * no errors.
+ */
+const InvalidConfigError = function(reasons: ErrorObject[] | null): void {
+    ;(this.emitter as EventEmitter).emit(INVALID_CONFIG_ERROR, reasons)
+}
+
+export class ProviderErrorEmitter implements Disposable {
+    protected emitter: EventEmitter
+    protected readonly sendConfigReadError: typeof ConfigReadError
+    protected readonly sendConflictingNameError: typeof ConflictingNameError
+    protected readonly sendExtendsUnknownPluginError: typeof ExtendsUnknownPluginError
+    protected readonly sendInvalidConfigError: typeof InvalidConfigError
+
+    constructor() {
+        this.emitter = new EventEmitter()
+        this.sendConfigReadError = ConfigReadError.bind(this)
+        this.sendConflictingNameError = ConflictingNameError.bind(this)
+        this.sendExtendsUnknownPluginError = ExtendsUnknownPluginError.bind(this)
+        this.sendInvalidConfigError = InvalidConfigError.bind(this)
+    }
+
+    dispose(): void {
+        this.emitter.removeAllListeners()
+    }
+
+    onConfigReadError(listener: typeof ConfigReadError): void {
+        this.emitter.on(CONFIG_READ_ERROR, listener)
+    }
+
+    onConflictingNameError(listener: typeof ConflictingNameError): void {
+        this.emitter.on(CONFLICTING_NAME_ERROR, listener)
+    }
+
+    onExtendsUnknownPluginError(listener: typeof ExtendsUnknownPluginError): void {
+        this.emitter.on(EXTENDS_UNKNOWN_PLUGIN_ERROR, listener)
+    }
+
+    onInvalidConfigError(listener: typeof InvalidConfigError): void {
+        this.emitter.on(INVALID_CONFIG_ERROR, listener)
+    }
+}
