9a32a9686bf1ad39e3c1affbe6ea2835dc2af0f5
Evan Hall <evan.tom.hall@gmail.com>
2020-02-22 12:57:10 -0500

    Exposed antlr4ts.tree.ParseTreeWalker

--- server/@antlr4-tsplang/src/index.ts
+++ server/@antlr4-tsplang/src/index.ts
@@ -15,7 +15,7 @@
  */
 // ANTLR4 Exports
 export { ANTLRInputStream, CommonTokenStream, ParserRuleContext } from "antlr4ts"
-export { ParseTree, TerminalNode } from "antlr4ts/tree"
+export { ParseTree, ParseTreeWalker, TerminalNode } from "antlr4ts/tree"
 
 // Overrides
 export { ExtendedTspDocLexer as TspDocLexer } from "./extendedTspDocLexer"
